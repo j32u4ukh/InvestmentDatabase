2e42a0cf0345bf5d298de333c6484892cd7d27ed
j32u4ukh <j32u4ukh@gmail.com>
2021-07-16 09:54:36 +0800

    實作 insert, select, update

--- database.php
+++ database.php
@@ -4,7 +4,7 @@
 		$user = "id17236763_j32u4ukh";
 		$password = ">_AJeTJOD#bH_l2(";
 		$database = "id17236763_stock_data";		
-		$database = new Database($server, $user, $password, $database);
+		$database = new Database("localhost", "id17236763_j32u4ukh", ">_AJeTJOD#bH_l2(", "id17236763_stock_data");
 		
 		return $database;
 	}
@@ -31,7 +31,7 @@
 		// 建立跟資料庫的連接，並設定語系是萬國語言以支援中文
 		private function connectPDO($server, $user, $password, $database){		
 			try {
-				$this->db = new PDO("mysql:host=$server; charset=utf8mb4; dbname=$database", self::user, self::password);
+				$this->db = new PDO("mysql:host=$server; charset=utf8mb4; dbname=$database", $user, $password);
 
 				// set the PDO error mode to exception
 				$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -51,43 +51,72 @@
 				return $result->fetchAll(); 
 				
 			} catch(PDOException $e) {
-				$this->error_message = '<p class="bg-danger">'. $e->getMessage() . '</p>';
+				$this->error_message = "<p class='bg-danger'>$e->getMessage()</p>";
+				echo $this->error_message;
+			}
+		}
+		
+		// 自己實作的讀取資料庫
+		public function select($table, $columns=array(), $n_limit=5){
+			if(count($columns) == 0){
+				$format_columns = "*";
+			}else{
+				$format_columns = join(",", $columns);
+			}
+			
+			$this->sql = "SELECT $format_columns FROM $table LIMIT $n_limit";
+			
+			try {
+				$result = $this->db->prepare($this->sql);
+				$result->execute();
+				
+				return $result->fetchAll();
+				
+			} catch(PDOException $e) {
+				$this->error_message = "<p class='bg-danger'> $e->getMessage() </p>";
 			}
 		}
 		
 		/**
 		 * 這段用來讀取資料庫中的資料，回傳的是陣列資料
 		 */
-		public function query($table, $condition, $order_by, $fields, $limit, $data_array){
+		public function query($table, $condition=array(), $order_by="DESC", $fields="*", $n_limit=5){
 			if(empty($table)){
 				return false;
 			}
 			
-			if(!isset($data_array) OR count($data_array) == 0){
-				return false;
-			}
+			// if(!isset($data_array) OR count($data_array) == 0){
+				// return false;
+			// }
 			
-			if(is_numeric($limit)){
-				$limit = "LIMIT " . $limit;
+			if(is_numeric($n_limit)){
+				if($n_limit > 0){
+					$limit = "LIMIT $n_limit";
+				}else{
+					$limit = "";
+				}
+			}else{
+				$limit = "";
 			}
 			
 			if(empty($condition)){
 				$condition = 1;
 			}
 			
-			if(empty($order_by)){
-				$order_by = 1;
-			}
+			// if(empty($order_by)){
+				// $order_by = 1;
+			// }
 			
-			if(empty($fields)){
-				$fields = "*";
-			}
+			// if(empty($fields)){
+				// $fields = "*";
+			// }
 			
 			$this->sql = "SELECT $fields FROM $table WHERE $condition ORDER BY $order_by $limit";
 			
 			try {
 				$result = $this->db->prepare($this->sql);
-				$result->execute($data_array);
+				// $result->execute($data_array);
+				$result->execute();
 				
 				return $result->fetchAll();
 				
@@ -98,6 +127,7 @@
 
 		/**
 		 * 這段可以新增資料庫中的資料，並把最後一筆的 ID 存到變數中，可以用 getLastId() 取出
+		 目前查到的資訊是 PDO 無法同時多筆寫入，待確認
 		 */
 		public function insert($table = null, $data_array = array()) {
 			if($table === null){
@@ -119,9 +149,13 @@
 			
 			$columns = join(",", $tmp_col);
 			$data = join(",", $tmp_dat);
-
+			echo "<p>columns: $columns</p>";
+			echo "<p>data: $data</p>";
+			echo "<p>prepare_array: $prepare_array</p>";
+			
 			// INSERT INTO table_name (KEY1, KEY2) VALUES (:KEY1, :KEY2);
 			$this->sql = "INSERT INTO $table ( $columns ) VALUES ( $data )";
+			echo "<p>sql: $this->sql</p>";
 			$result = $this->db->prepare($this->sql);
 			
 			// $prepare_array = {":KEY1": [value1, value2, value3], ":KEY2": [value1, value2, value3]}
@@ -133,37 +167,42 @@
 		/**
 		 * 這段可以更新資料庫中的資料
 		 */
-		public function update($table = null, $data_array = null, $key_column = null, $id = null) {
-			if($table == null){
-				return false;
-			}
-			
-			if($id == null){
-				return false;
-			}
-			
-			if($key_column == null){
-				return false;
-			}
-			
+		public function update($table, $data_array, $key_column, $value) {		
 			if(count($data_array) == 0){
 				return false;
 			}
-
-			$setting_list = "";
 			
-			for ($xx = 0; $xx < count($data_array); $xx++) {
-				list($key, $value) = each($data_array);
-				// $setting_list .= $key . "=" . ':'.$key;
-				$setting_list .= "$key=:$key";
-				
-				if ($xx != count($data_array) - 1){
-					$setting_list .= ",";
+			// if($id == null){
+				// return false;
+			// }
+						
+			$setting_list = array();
+			
+			foreach ($data_array as $key => $value) {
+				if($key == $key_column){
+					continue;
 				}
+				
+				$setting_list[] = "$key=:$key";
 			}
 			
-			$data_array[$key_column] = $id;
-			$this->sql = "UPDATE $table SET $setting_list WHERE $key_column = :$key_column";
+			$setting = join(",", $setting_list);
+			echo "<p>setting: $setting</p>";
+			
+			// for ($xx = 0; $xx < count($data_array); $xx++) {
+				// list($key, $value) = each($data_array);
+				// $setting_list .= $key . "=" . ':'.$key;
+				// $setting_list .= "$key=:$key";
+				
+				// if ($xx != count($data_array) - 1){
+					// $setting_list .= ",";
+				// }
+			// }
+			
+			// $data_array[$key_column] = $value;
+			$this->sql = "UPDATE $table SET $setting WHERE $key_column = :$key_column";
+			echo "<p>sql: $this->sql</p>";
+			
 			$result = $this->db->prepare($this->sql);                       
 			$result->execute($data_array);
 		}
@@ -248,8 +287,7 @@
 		 * @param string $error_message
 		 * 記下錯誤訊息到物件變數內
 		 */
-		private function setErrorMessage($error_message)
-		{
+		private function setErrorMessage($error_message){
 			$this->error_message = $error_message;
 		}
 	}
--- index.php
+++ index.php
@@ -7,10 +7,14 @@
         <p>// TODO: 首頁呈現現有庫存 & 最近幾筆交易紀錄以及整體策略表現</p>
         <p>// TODO: 各個投資者利用帳密可登入個別頁面，看到自己的資金變化</p>
 		<?php
-			include "connect.php";
-			// $conn = connectPDO();
-			$connect = new Connect();
-			$conn = $connect->connectMysqliConnect();
+			include "database.php";
+			$db = connectToStockData();
+			// $db->insert("STOCK_LIST", array("STOCK_ID"=> "2330", "NAME"=> "", "PRICE"=> "614.00"));
+			$db->update("STOCK_LIST", array("STOCK_ID"=> "2330", "NAME"=> "台積電", "PRICE"=> "615.00"), "STOCK_ID", "2330");
+			
+			$result = $db->select("STOCK_LIST");
+			$json_result = json_encode($result);
+			echo "<p>$json_result</p>";
 		?>
     </body>
 </html>
